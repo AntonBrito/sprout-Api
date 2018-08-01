7985c69aa3031e7f369499341e2f928eef41f413
AntonBrito <tonybrito85@gmail.com>
2018-08-01 18:55:30 -0400

    CleanUp some code, testing users route

--- server.js
+++ server.js
@@ -1,29 +1,29 @@
 // This is our entryPoint
 
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
-const users = require('.routes/api/users');
-const profile = require('.routes/api/profile')
-const posts = require('.routes/api.posts');
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
 
 const app = express();
 
 // DB Config
-const db = require('./config/keys').mongoURI;
+const db = require("./config/keys").mongoURI;
 
 // connect to mongoDB
 mongoose
-    .connect(db)
-    .then(() => console.log('Mongo Connected'))
-    .catch(err => console.log(err));
+  .connect(db)
+  .then(() => console.log("Mongo Connected"))
+  .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello'));
+app.get("/", (req, res) => res.send("Hello"));
 
 // Use Routes
-app.use('/api/users', users);
-app.use('/api/profile', profile);
-app.use('/api/posts', posts);
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
 
