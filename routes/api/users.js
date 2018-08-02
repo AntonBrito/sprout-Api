48d22faa2c326fc89df771d86719b22afa399db4
AntonBrito <tonybrito85@gmail.com>
2018-08-01 20:03:40 -0400

    User Registration. added user security credentials

--- routes/api/users.js
+++ routes/api/users.js
@@ -33,6 +33,17 @@ router.post("/register", (req, res) => {
         avatar,
         password: req.body.password
       });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
     }
   });
 });
